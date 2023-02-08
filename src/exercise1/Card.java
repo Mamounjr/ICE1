189c5a6f81f0847c3b84a3d74e39f9fc9612456e
Mamoun <diabmam@sheridancollege.ca>
2023-02-08 16:23:31 -0500

    new commit

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,6 +7,7 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
+ * @modifer Mamoun Diab
  * @author dancye
  * @author Paul Bonenfant May 2020
  */
@@ -14,10 +15,33 @@ public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
+   
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
+     *  Card[] hand = new Card[7];
+        hand[0]= new Card();
+        hand[0].setValue(1);
+        hand[0].setSuit("Hearts");        
+        hand[1]= new Card();
+        hand[1].setValue(2);
+        hand[1].setSuit("Clubs");        
+        hand[2]= new Card();
+        hand[2].setValue(3);
+        hand[2].setSuit("Diomonds");
+        hand[3]= new Card();
+        hand[3].setValue(4);
+        hand[3].setSuit("Spades");
+        hand[4]= new Card();
+        hand[4].setValue(5);
+        hand[4].setSuit("Clubs");
+        hand[5]= new Card();
+        hand[5].setValue(12);
+        hand[5].setSuit("Hearts");
+        hand[6]= new Card();
+        hand[6].setValue(11);
+        hand[6].setSuit("Hearts");
      */
     public String getSuit() {
         return suit;
